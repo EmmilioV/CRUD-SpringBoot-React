2f544c31a67aa1531bc4e971a9f6a38af1ef6f7f
EmmilioV <emmiliov.p@gmail.com>
2021-11-26 06:16:32 -0500

    first commit

---
+++ crud-springboot-part/src/test/java/com/sofka/crudspringbootpart/CrudSpringbootPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.crudspringbootpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
